926855878450e1fcb8db6a3397e290b788cc06cd
mbhatley <102679325+mbhatley@users.noreply.github.com>
2025-01-06 15:45:45 -0600

    updated

---
+++ helper_lists.py
@@ -0,0 +1,27 @@
+# For Mac Devices:
+chromeDriverPath = "/usr/local/bin/chromedriver"
+
+# For Windows Devices: update as needed
+# chromeDriverPath = "C:\\Program Files\\chromedriver_win32\\chromedriver.exe
+
+# User agents taken from https://www.useragents.me/#latest-windows-desktop-useragents
+# Check every quarter for updates
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/125.0.2535.92',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:115.0) Gecko/20100101 Firefox/115.0',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/111.0.0.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:127.0) Gecko/20100101 Firefox/127.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/111.0.0.0',
+    'Mozilla/5.0 (iPad; CPU OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/127.0 Mobile/15E148 Safari/605.1.15',
+    'Mozilla/5.0 (iPad; CPU OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/100.0.4896.127 Safari/537.36',
+    'Bingbot/2.0',
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/116.0.1938.76 Safari/537.36'
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/100.0.4896.127 Safari/537.36'
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/116.0.1938.76 Safari/537.36'
+    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm) Chrome/112.0.0.0 Safari/537.36'
+]
--- nasdaq_ipo_list_scraper.py
+++ nasdaq_ipo_list_scraper.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import pandas as pd
 import bs4
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from helper_lists import chromeDriverPath
+from selenium.webdriver.common.by import By
 
 service = Service(chromeDriverPath)
 options = Options()
@@ -15,32 +12,111 @@ options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 options.add_argument("--incognito")
 driver = webdriver.Chrome(service=service, options=options)
 
-ipos = pd.read_csv("/Users/michael/Trading/IPO/ipo_list.csv")
+ipo_priced = pd.read_csv("/Users/michael/Documents/Trading/IPO/ipo_priced.csv")
+ipo_upcoming = pd.read_csv("/Users/michael/Documents/Trading/IPO/ipo_upcoming.csv")
+ipo_filings = pd.read_csv("/Users/michael/Documents/Trading/IPO/ipo_filings.csv")
+ipo_withdrawn = pd.read_csv("/Users/michael/Documents/Trading/IPO/ipo_withdrawn.csv")
 
-soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+# Find all containers with the shadow-root tables
+containers = driver.find_elements(By.CSS_SELECTOR, "div.jupiter22-ipo-calendar__priced_table")
+# Iterate over each container and extract data
+for container in containers:
+    # Locate the shadow host (nsdq-table-sort)
+    shadow_host = container.find_element(By.CSS_SELECTOR, "nsdq-table-sort")
+    # Access the shadow root using JavaScript
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+    # Find the table inside the shadow root
+    table_rows = shadow_root.find_elements(By.CSS_SELECTOR, "div[role='row']")
 
-# table = soup.find('table', {'class': 'market-calendar-table__table'})
-
-tables = soup.find_all('table', {'class': 'market-calendar-table__table'})
-
-if len(tables) >= 2:
-    # Extract data from the second table
-    table = tables[2]
-
-    data = []
-
-    for row in table.find_all('tr'):
-        # Extracting data from the row cells
-        row_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
+    data_price = []
+    # Extract and print table data
+    for row in table_rows:
+        cells = row.find_elements(By.CSS_SELECTOR, "div[role='cell']")
+        row_data = [cell.text for cell in cells]
         if row_data:  # Exclude empty rows
-            data.append(row_data)
+            data_price.append(row_data)
 
-    df = pd.DataFrame(data[1:], columns=data[0])  # Skipping the first row as it contains headers
+    df_priced = pd.DataFrame(data_price[1:], columns=data_price[0])  # Skipping the first row as it contains headers
+    df_priced = pd.concat([ipo_priced, df_priced], ignore_index=True)
+    df_priced['Company Name'] = df_priced['Company Name'].fillna('')
+    df_priced = df_priced.drop_duplicates(subset=['Company Name'], keep='last')
+    print(df_priced)
+    df_priced.to_csv("/Users/michael/Documents/Trading/IPO/ipo_priced.csv", index=False, encoding='utf-8-sig')
 
-    df = pd.concat([ipos, df], ignore_index=True)
+# Find all containers with the shadow-root tables
+containers = driver.find_elements(By.CSS_SELECTOR, "div.jupiter22-ipo-calendar__upcoming_table")
+# Iterate over each container and extract data
+for container in containers:
+    # Locate the shadow host (nsdq-table-sort)
+    shadow_host = container.find_element(By.CSS_SELECTOR, "nsdq-table-sort")
+    # Access the shadow root using JavaScript
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+    # Find the table inside the shadow root
+    table_rows = shadow_root.find_elements(By.CSS_SELECTOR, "div[role='row']")
 
-    df = df.drop_duplicates()
+    data_upcoming = []
+    # Extract and print table data
+    for row in table_rows:
+        cells = row.find_elements(By.CSS_SELECTOR, "div[role='cell']")
+        row_data = [cell.text for cell in cells]
+        if row_data:  # Exclude empty rows
+            data_upcoming.append(row_data)
 
-    print(df)
+    df_upcoming = pd.DataFrame(data_upcoming[1:], columns=data_upcoming[0])  # Skipping the first row as it contains headers
+    df_upcoming = pd.concat([ipo_upcoming, df_upcoming], ignore_index=True)
+    df_upcoming['Company Name'] = df_upcoming['Company Name'].fillna('')
+    df_upcoming = df_upcoming.drop_duplicates(subset=['Company Name'], keep='last')
+    print(df_upcoming)
+    df_upcoming.to_csv("/Users/michael/Documents/Trading/IPO/ipo_upcoming.csv", index=False, encoding='utf-8-sig')
 
-    df.to_csv("/Users/michael/Trading/IPO/ipos.csv", index=False, encoding='utf-8-sig')
+    # Find all containers with the shadow-root tables
+containers = driver.find_elements(By.CSS_SELECTOR, "div.jupiter22-ipo-calendar__filings")
+# Iterate over each container and extract data
+for container in containers:
+    # Locate the shadow host (nsdq-table-sort)
+    shadow_host = container.find_element(By.CSS_SELECTOR, "nsdq-table-sort")
+    # Access the shadow root using JavaScript
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+    # Find the table inside the shadow root
+    table_rows = shadow_root.find_elements(By.CSS_SELECTOR, "div[role='row']")
+
+    data_filings = []
+    # Extract and print table data
+    for row in table_rows:
+        cells = row.find_elements(By.CSS_SELECTOR, "div[role='cell']")
+        row_data = [cell.text for cell in cells]
+        if row_data:  # Exclude empty rows
+            data_filings.append(row_data)
+
+    df_filings = pd.DataFrame(data_filings[1:], columns=data_filings[0])  # Skipping the first row as it contains headers
+    df_filings = pd.concat([ipo_filings, df_filings], ignore_index=True)
+    df_filings['Company Name'] = df_filings['Company Name'].fillna('')
+    df_filings = df_filings.drop_duplicates(subset=['Company Name'], keep='last')
+    print(df_filings)
+    df_filings.to_csv("/Users/michael/Documents/Trading/IPO/ipo_filings.csv", index=False, encoding='utf-8-sig')
+
+    # Find all containers with the shadow-root tables
+containers = driver.find_elements(By.CSS_SELECTOR, "div.jupiter22-ipo-calendar__withdrawn_table")
+# Iterate over each container and extract data
+for container in containers:
+    # Locate the shadow host (nsdq-table-sort)
+    shadow_host = container.find_element(By.CSS_SELECTOR, "nsdq-table-sort")
+    # Access the shadow root using JavaScript
+    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+    # Find the table inside the shadow root
+    table_rows = shadow_root.find_elements(By.CSS_SELECTOR, "div[role='row']")
+
+    data_withdrawn = []
+    # Extract and print table data
+    for row in table_rows:
+        cells = row.find_elements(By.CSS_SELECTOR, "div[role='cell']")
+        row_data = [cell.text for cell in cells]
+        if row_data:  # Exclude empty rows
+            data_withdrawn.append(row_data)
+
+    df_withdrawn = pd.DataFrame(data_withdrawn[1:], columns=data_withdrawn[0])  # Skipping the first row as it contains headers
+    df_withdrawn = pd.concat([ipo_withdrawn, df_withdrawn], ignore_index=True)
+    df_withdrawn['Company Name'] = df_withdrawn['Company Name'].fillna('')
+    df_withdrawn = df_withdrawn.drop_duplicates(subset=['Company Name'], keep='last')
+    print(df_withdrawn)
+    df_withdrawn.to_csv("/Users/michael/Documents/Trading/IPO/ipo_withdrawn.csv", index=False, encoding='utf-8-sig')
